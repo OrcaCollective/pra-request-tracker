80d04940052489024a4de1ea82902d8ca06dd023
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-29 16:20:22 -0700

    Fix various local env issues and update the README (#1)
    * Fix timezone
    
    * Move envs to correct location
    
    * Update README with better instructions
    
    * Convert README to markdown
    
    * Add CI
    
    * Added some clarity for README instructions
    
    Got a little confused with the order of operations described in the README so I updated it to add some clarification around the need to attempt logging in to the web app after you run the createsuperuser command, as well as an alternative option for viewing the logs of the django service a little more easily.
    
    * Run pre-commit
    
    Co-authored-by: Marcus Puckett <mpuckett259@gmail.com>

--- config/settings/production.py
+++ config/settings/production.py
@@ -77,7 +77,8 @@ TEMPLATES[-1]["OPTIONS"]["loaders"] = [  # type: ignore[index] # noqa F405
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
-    "DJANGO_DEFAULT_FROM_EMAIL", default="PRA Request Tracker <noreply@twitter.com/TechBlocSEA>"
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="PRA Request Tracker <noreply@twitter.com/TechBlocSEA>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)

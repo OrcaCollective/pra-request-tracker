fa0e5ab059b6c65ba291b6abd7d1efd8860d0916
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-06-30 12:46:58 -0700

    Add admin for the new models

--- pra_request_tracker/foia/admin.py
+++ pra_request_tracker/foia/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin  # noqa
 
-# Register your models here.
+from pra_request_tracker.foia.models import FoiaRequest
+from pra_request_tracker.foia.models import FoiaRequestFile
+from pra_request_tracker.foia.forms import FoiaRequestForm
+from pra_request_tracker.foia.forms import FoiaRequestFileForm
+
+
+@admin.register(FoiaRequest)
+class FoiaRequestAdmin(admin.ModelAdmin):
+    form = FoiaRequestForm
+
+
+@admin.register(FoiaRequestFile)
+class FoiaRequestFileAdmin(admin.ModelAdmin):
+    form = FoiaRequestFileForm
--- pra_request_tracker/foia/models.py
+++ pra_request_tracker/foia/models.py
@@ -6,7 +6,10 @@ from pra_request_tracker.users.models import User
 class FoiaRequest(models.Model):
     requester = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
-    agency = models.TextField()
+    agency = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"FoiaRequest({self.id}) to {self.agency}"
 
 
 class FoiaRequestFile(models.Model):
@@ -16,3 +19,6 @@ class FoiaRequestFile(models.Model):
     )
     url = models.URLField()
     description = models.TextField(null=True)
+
+    def __str__(self):
+        return f"FoiaRequestFile({self.id}) for {self.request}"

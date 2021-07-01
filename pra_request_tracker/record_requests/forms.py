72afbaa30a51104a70cfa4841d30f937072dafb2
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-01 05:59:34 -0700

    Remove whitespace between class attributes

--- pra_request_tracker/record_requests/forms.py
+++ pra_request_tracker/record_requests/forms.py
@@ -8,19 +8,16 @@ from .models import RecordRequestFile
 class AgencyForm(forms.ModelForm):
     class Meta:
         model = Agency
-
         fields = ("name",)
 
 
 class RecordRequestForm(forms.ModelForm):
     class Meta:
         model = RecordRequest
-
         fields = ("requester", "agency", "title", "description", "status")
 
 
 class RecordRequestFileForm(forms.ModelForm):
     class Meta:
         model = RecordRequestFile
-
         fields = ("request", "title", "description", "file")

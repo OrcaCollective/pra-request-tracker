d93478da43d90e6b4aeb25dfcc00d85d5f50a196
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-04 18:01:22 -0700

    Flesh out the model admins (#10)

--- pra_request_tracker/record_requests/admin.py
+++ pra_request_tracker/record_requests/admin.py
@@ -11,13 +11,25 @@ from .forms import RecordRequestFileForm
 @admin.register(Agency)
 class AgencyAdmin(admin.ModelAdmin):
     form = AgencyForm
+    list_display = ("name",)
 
 
 @admin.register(RecordRequest)
 class RecordRequestAdmin(admin.ModelAdmin):
     form = RecordRequestForm
+    list_display = (
+        "title",
+        "agency",
+        "tracking_number",
+        "status",
+        "filed_at",
+        "estimated_response_date",
+        "last_communication_date",
+        "updated_at",
+    )
 
 
 @admin.register(RecordRequestFile)
 class RecordRequestFileAdmin(admin.ModelAdmin):
     form = RecordRequestFileForm
+    list_display = ("title", "request")

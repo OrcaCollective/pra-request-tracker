e3053a17c6a1a459dedf5e2cc8486020c59259d6
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-15 12:24:40 -0700

    Avoid a dangerous del
    Co-authored-by: Madison Swain-Bowden <bowdenm@spu.edu>

--- pra_request_tracker/pra_request_tracker/settings_test.py
+++ pra_request_tracker/pra_request_tracker/settings_test.py
@@ -2,4 +2,5 @@ from pra_request_tracker.settings import *  # noqa: F403
 
 
 MEDIA_ROOT = APPS_DIR / "media"  # noqa: F405
-del DEFAULT_FILE_STORAGE  # noqa: F821
+if "DEFAULT_FILE_STORAGE" in globals():
+    del DEFAULT_FILE_STORAGE  # noqa: F821

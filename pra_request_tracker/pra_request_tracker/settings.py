3fc6f3348190346d9c47c63ebd38ada674e40ab6
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-15 06:47:49 -0700

    Get it running locally

--- pra_request_tracker/pra_request_tracker/settings.py
+++ pra_request_tracker/pra_request_tracker/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+
+env = environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-__jx4b3%kaqx-0rmm^%w79n4hia75_dj9+(fs&2)k7d25pn2eq"
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = env.list("DJANGO_ALLOWED_HOSTS")
 
 
 # Application definition
@@ -74,12 +77,8 @@ WSGI_APPLICATION = "pra_request_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 
 # Password validation

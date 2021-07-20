97836ece0d438c5bb72d142c00b3d70738c7a420
sarayourfriend <24264157+sarayourfriend@users.noreply.github.com>
2021-07-19 19:37:07 -0700

    Add custom admin actions

--- pra_request_tracker/pra_request_tracker/record_requests/admin.py
+++ pra_request_tracker/pra_request_tracker/record_requests/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.http import QueryDict
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .forms import AgencyForm, RecordRequestFileForm, RecordRequestForm
 from .models import Agency, RecordRequest, RecordRequestFile
@@ -7,8 +10,21 @@ from .models import Agency, RecordRequest, RecordRequestFile
 @admin.register(Agency)
 class AgencyAdmin(admin.ModelAdmin):
     form = AgencyForm
-    list_display = ("name",)
+    list_display = ("name", "agency_actions")
     search_fields = ("name",)
+
+    def agency_actions(self, obj):
+        query = QueryDict(mutable=True)
+        query.update({"agency": obj.pk})
+        return format_html(
+            '<a class="button custom-action" href="{}">Add request</a>'
+            '<a class="button custom-action" href="{}">View requests</a>',
+            f'{reverse("admin:record_requests_recordrequest_add")}?{query.urlencode()}',
+            f'{reverse("admin:record_requests_recordrequest_changelist")}?{query.urlencode()}',
+        )
+
+    agency_actions.short_description = "Actions"
+    agency_actions.allow_tags = True
 
 
 @admin.register(RecordRequest)
@@ -16,6 +32,7 @@ class RecordRequestAdmin(admin.ModelAdmin):
     form = RecordRequestForm
     list_display = (
         "title",
+        "record_request_actions",
         "agency",
         "tracking_number",
         "status",
@@ -26,6 +43,19 @@ class RecordRequestAdmin(admin.ModelAdmin):
     )
     search_fields = ("title", "agency", "tracking_number")
     autocomplete_fields = ("agency", "requester")
+
+    def record_request_actions(self, obj):
+        query = QueryDict(mutable=True)
+        query.update({"request": obj.pk})
+        return format_html(
+            '<a class="button custom-action" href="{}">Add file</a>'
+            '<a class="button custom-action" href="{}">View files</a>',
+            f'{reverse("admin:record_requests_recordrequestfile_add")}?{query.urlencode()}',
+            f'{reverse("admin:record_requests_recordrequestfile_changelist")}?{query.urlencode()}',
+        )
+
+    record_request_actions.short_description = "Actions"
+    record_request_actions.allow_tags = True
 
 
 @admin.register(RecordRequestFile)
